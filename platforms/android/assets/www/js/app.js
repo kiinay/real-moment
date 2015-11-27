68deaac38c0851cee3befce8278f84a463c1d91f
Yannick PLACERDAT <yannick.placerdat@gmail.com>
2015-11-27 15:50:18 +0100

    First commit !

---
+++ platforms/android/assets/www/js/app.js
@@ -0,0 +1,92 @@
+var pictureSource;   // picture source
+var destinationType; // sets the format of returned value 
+
+// Wait for Cordova to connect with the device
+//
+document.addEventListener('deviceready',onDeviceReady,false);
+
+// Cordova is ready to be used!
+//
+function onDeviceReady() {
+  pictureSource=navigator.camera.PictureSourceType;
+  destinationType=navigator.camera.DestinationType;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoDataSuccess(imageData) {
+  // Uncomment to view the base64 encoded image data
+  // console.log(imageData);
+
+  // Get image handle
+  //
+  var smallImage = document.getElementById('smallImage');
+
+  // Unhide image elements
+  //
+  smallImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  smallImage.src = "data:image/jpeg;base64," + imageData;
+}
+
+// Called when a photo is successfully retrieved
+//
+function onPhotoURISuccess(imageURI) {
+  // Uncomment to view the image file URI 
+  console.log(imageURI);
+
+  // Get image handle
+  //
+  var largeImage = document.getElementById('largeImage');
+
+  // Unhide image elements
+  //
+  largeImage.style.display = 'block';
+
+  // Show the captured photo
+  // The inline CSS rules are used to resize the image
+  //
+  largeImage.src = imageURI;
+}
+
+// A button will call this function
+//
+function capturePhoto() {
+  // Take picture using device camera and retrieve image as base64-encoded string
+  alert('Photo Prise !');
+  navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 50,
+    destinationType: destinationType.DATA_URL });
+}
+
+$('#capture').on('click', capturePhoto());
+
+// A button will call this function
+//
+function getPhoto(source) {
+  // Retrieve image file location from specified source
+  navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50, 
+    destinationType: destinationType.FILE_URI,
+    sourceType: source });
+}
+
+// Called if something bad happens.
+// 
+function onFail(message) {
+  alert('Failed because: ' + message);
+}
+
+
+
+
+
+var app = angular.module('app', ['ngRoute']);
+
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/picture', {templateUrl: 'partials/picture.html'})
+    .when('/galery', {templateUrl: 'partials/galery.html'})
+    .otherwise({redirectTo: '/picture'})
+});
